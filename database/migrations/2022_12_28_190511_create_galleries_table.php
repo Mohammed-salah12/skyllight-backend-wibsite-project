5af60ffffea9e12a7410558d567e863e3e49ac20
Khs0819 <ohgf0819@gmail.com>
2022-12-28 21:10:50 +0200

    first

--- database/migrations/2022_12_28_190511_create_galleries_table.php
+++ database/migrations/2022_12_28_190511_create_galleries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGalleriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('galleries', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('mobile')->unique();
-            $table->string('image')->nullable();
-            $table->string('username');
-            $table->string('password');
+            $table->string('title');
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('galleries');
     }
 }
