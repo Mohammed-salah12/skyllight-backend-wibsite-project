13cdfb1e323aad616865686bafacd8d7fa030998
JumanaAlkfarna <jumanawael4@gmail.com>
2022-12-28 21:21:35 +0200

    MigrationsAll

---
+++ app/Models/Service.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    public function Service_images(){
+        return $this->hasMany(Service_image::class);
+    }
+
+
+    public function Service_descriptions(){
+        return $this->hasMany(Service_description::class);
+    }
+}
---
+++ app/Models/Service_description.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service_description extends Model
+{
+    use HasFactory;
+
+    public function service() {
+        return $this->belongsTo(Service::class);
+    }
+}
