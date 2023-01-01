c0d5a4c71ea94c755c8fc7e458319464744a08d1
Khs0819 <ohgf0819@gmail.com>
2023-01-01 12:12:59 +0200

    route1

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('admin', 'cms.parent');
+    Route::prefix('cms/admin/')->group(function(){
+        Route::view('' , 'cms.parent');
+        Route::view('temp' , 'cms.temp');
+        Route::resource('services' , ServiceController::class);
+    Route::post('update-services/{id}' , [ServiceController::class , 'update'])->name('update-services');
+
+    Route::resource('service_images' , Service_ImageController::class);
+    Route::post('update-service_images/{id}' , [Service_ImageController::class , 'update'])->name('update-service_images');
+
+    Route::resource('service_descriptions' , Service_descriptionController::class);
+    Route::post('update-service_descriptions/{id}' , [Service_descriptionController::class , 'update'])->name('update-service_descriptions');
+
+    });
