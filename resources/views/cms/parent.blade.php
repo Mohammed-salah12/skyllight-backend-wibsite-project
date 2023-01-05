ff150fdfd25ffdbd3d279627038cc6e7dcc845af
Khs0819 <ohgf0819@gmail.com>
2023-01-05 11:14:13 +0200

    pulll

--- resources/views/cms/parent.blade.php
+++ resources/views/cms/parent.blade.php
@@ -257,7 +257,7 @@
                     </a>
                   </li>
                 <li class="nav-item">
-                    <a href="{{ route('partners_images.index') }}" class="nav-link">
+                    <a href="{{ route('partner_images.index') }}" class="nav-link">
                         <i class="fa-solid fa-plus"></i>
                       <p>اضافة شريك</p>
                     </a>
